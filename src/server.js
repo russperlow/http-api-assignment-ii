bd11f7b4d2a30501f56d68b0434779af387d0d87
Russ Perlow <rep4975@rit.edu>
2019-09-27 12:22:21 -0400

    Updated server.js from assigment 1 to 2

--- src/server.js
+++ src/server.js
@@ -1,22 +1,59 @@
 const http = require('http');
 const url = require('url');
+const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const urlStruct = {
-    '/': htmlHandler.getIndex
+const handlePost = (request, response, parsedUrl) => {
+    if(parsedUrl.pathName == '/addUser'){
+        const body = [];
+
+        request.on('error', (err) => {
+            console.log(err);
+            response.statusCode = 400;
+            response.end();
+        });
+        request.on('data', (data) => {
+            body.push(data)
+        });
+        request.on('end', () => {
+            const bodyString = Buffer.concat(body).toString();
+            const bodyParams = query.parse(bodyString);
+            jsonHandler.addUser(request, response, bodyParams);
+        })
+    }
+};
+
+const handleHead = (request, response, parsedUrl) => {
+    if(parsedUrl.pathName == '/getUsers'){
+        jsonHandler.getUsers(request, response);
+    }else{
+        jsonHandler.notReal(request, response);
+    }
+}
+
+const handleGet = (request, response, parsedUrl) => {
+    if(parsedUrl.pathName == '/'){
+        htmlHandler.getIndex(request, response);
+    }else if(parsedUrl.pathName == '/style.css'){
+        htmlHandler.getCSS(request, response)
+    }else if(parsedUrl.pathName == '/getUsers'){
+        htmlHandler.getUsers(request, response);
+    }else{
+        jsonHandler.notReal(request, response);
+    }
 }
 
 const onRequest = (request, response) => {
     const parsedUrl = url.parse(request.url);
-    const params = query.parse(parsedUrl.query);
-    const acceptedTypes = request.headers.accept.split(',');
 
-    if(urlStruct[parsedUrl.pathName]){
-        urlStruct[parsedUrl.pathname](request, response, paramas, acceptedTypes);
+    if(request.method == 'POST'){
+        handlePost(request, response, parsedUrl);
+    }else if(request.method == 'HEAD'){
+        handleHead(request, response, parsedUrl);
     }else{
-        urlStruct.notFound(request, response, params, acceptedTypes);
+        handleGet(request, response, parsedUrl);
     }
 }
 
